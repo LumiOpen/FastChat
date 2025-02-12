eed7df75ffa9ec4c20d6e38768d5a4bfe68ca326
ezosa <elaine.zosa@gmail.com>
2025-02-12 11:49:51 +0200

    add support for Finnish Llama CPT

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -2568,10 +2568,21 @@ class Llama3BaseAdapter(BaseModelAdapter):
         return "llama-3.1-base" in model_path.lower()
 
     def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("llama-3.1")
+
+
+class FinnishLlama3BaseAdapter(BaseModelAdapter):
+    """The model adapter for Mistral CPT"""
+
+    def match(self, model_path: str):
+        return "finnish-llama-3.1" in model_path.lower()
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
         return get_conv_template("finnish-llama-3.1")
     
 # Note: the registration order matters.
 # The one registered earlier has a higher matching priority.
+register_model_adapter(FinnishLlama3BaseAdapter)
 register_model_adapter(FinnishAmberBaseAdapter)
 register_model_adapter(FinnishSalamandraBaseAdapter)
 register_model_adapter(FinnishMistralBaseAdapter)
